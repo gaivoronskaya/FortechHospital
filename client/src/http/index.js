7be50948cc49ae0bbb2f87df5c76d890e25b8fd2
Ekaterina Gaivoronskaya Ekaterina <Gaivoronskaya.fortech@gmail.com>
2024-08-20 13:03:11 +0300

    interceptor

--- client/src/http/index.js
+++ client/src/http/index.js
@@ -1,6 +1,3 @@
-//!!!!!!!!!!!!!!! Без библиотеки не могу решить проблему сохранения isAth. По этому данный код в данный момент бесполезен 
-
-
 import axios from "axios";
 import { refreshToken } from "../services/users";
 import { baseURL } from "../constants";
@@ -10,42 +7,39 @@ export const api = axios.create({
   withCredentials: true,
 });
 
-// api.interceptors.request.use((config) => {
-//   config.headers.Authorization = `Bearer ${localStorage.getItem(
-//     "accessToken"
-//   )}`;
+api.interceptors.request.use((config) => {
+  config.headers.Authorization = `Bearer ${localStorage.getItem(
+    "accessToken"
+  )}`;
 
-//   return config;
-// });
+  return config;
+});
 
-// api.interceptors.response.use(
-//   (response) => {
-//     return response;
-//   },
-//   async (error) => {
-//     const originalRequest = error.config;
-//     if (
-//       error.response &&
-//       error.response.status === 401 &&
-//       !originalRequest._retry
-//     ) {
-//       originalRequest._retry = true;
-//       try {
-//         const newAccessToken = await refreshToken();
-//         console.log("NEW", newAccessToken);
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    const originalRequest = error.config;
+    if (
+      error.response &&
+      error.response.status === 401 &&
+      !originalRequest._retry
+    ) {
+      originalRequest._retry = true;
+      try {
+        const newAccessToken = await refreshToken();
+        console.log("NEW", newAccessToken);
 
-//         localStorage.setItem("accessToken", newAccessToken);
-//         sessionStorage.setItem("accessToken", newAccessToken);
+        localStorage.setItem("accessToken", newAccessToken);
+        sessionStorage.setItem("accessToken", newAccessToken);
 
-//         // api.defaults.headers.common["Authorization"] = `Bearer ${newAccessToken}`;
-//         // originalRequest.headers["Authorization"] = `Bearer ${newAccessToken}`;
+        return api.request(originalRequest);
+      } catch (error) {
+        throw error;
+      }
+    }
+  }
+);
 
-//         return api.request(originalRequest);
-//       } catch (error) {
-//         throw error;
-//       }
-//     }
-//   }
-// );
-
-// export default api;
+export default api;
