a15e2e455fd1ac0cb843a67fbe20dcdef012ebc9
Ekaterina Gaivoronskaya Ekaterina <Gaivoronskaya.fortech@gmail.com>
2024-09-04 10:19:20 +0300

    delete comments

--- client/src/constants.js
+++ client/src/constants.js
@@ -25,30 +25,3 @@ export const valueSortedList = [
   {value: "ascending", label: "По возрастанию"},
   {value: "descending", label: "По убыванию"},
 ]
-
-// const start = 3;
-// const end = 5;
-
-// const sumNumers = (start, end) => {
-//   let sum = 0;
-//   let k = start;
-//   while (k <= end) {
-//     sum += k;
-//     k ++;
-//   }
-
-//   return sum;
-// }
-
-// console.log(sumNumers(start, end));
-
-const start = 3;
-const end = 5;
-
-const sumNumbers = (start, end) => {
-  let count = end - start + 1;
-  let result = ((start + end) * count) / 2;
-
-  return result;
-}
-console.log(sumNumbers(start, end));
