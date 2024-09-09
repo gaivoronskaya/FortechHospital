3ae6472d166ad6b51255d3a7bf936f084a254fa9
Ekaterina Gaivoronskaya Ekaterina <Gaivoronskaya.fortech@gmail.com>
2024-09-09 16:31:31 +0300

    final fix

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import HeaderContext from "../../context/header.js";
+import HeaderContext from "../../context/header";
 import {
   StyledHeaderContainer,
   StyledHeaderImage,
